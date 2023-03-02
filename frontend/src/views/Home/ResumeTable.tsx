d6d0586e289c0becec8ecb39b7f5faaa3ca87959
Ikuni17 <white.brad17@gmail.com>
2023-03-02 11:38:33 -0700

    Add simple state and fetching to Resume

--- frontend/src/views/Home/ResumeTable.tsx
+++ frontend/src/views/Home/ResumeTable.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
-import {HugoTitle} from '../../components';
+import React, {useEffect, useState} from 'react';
+import {HugoStack, HugoTable, HugoTitle} from '../../components';
 
 export const ResumeTable: React.FC = () => {
+  const [applications, setApplications] = useState<Application[]>();
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/application', {
+      mode: 'cors'
+    })
+      .then(r => {
+        if (r.ok) {
+          return r.json() as Promise<Application[]>;
+        }
+      })
+      .then(data => {
+        setApplications(data);
+      });
+  }, []);
+
   return (
-    <HugoTitle order={3} align="center">
-      {'Resume Application'}
-    </HugoTitle>
+    <>
+      <HugoTitle order={3} align="center">
+        {'Resume Application'}
+      </HugoTitle>
+      <HugoStack>
+        <HugoTable></HugoTable>
+      </HugoStack>
+    </>
   );
 };
