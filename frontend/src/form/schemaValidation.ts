d727ad88234f09255ba11a9f93105eabcee9922f
Ikuni17 <white.brad17@gmail.com>
2023-03-02 10:07:09 -0700

    Add min age validation

--- frontend/src/form/schemaValidation.ts
+++ frontend/src/form/schemaValidation.ts
@@ -1,8 +1,15 @@
+import dayjs from 'dayjs';
 import {object, string, date} from 'yup';
+
+const isMinimumAge = (date: Date) => {
+  return dayjs().diff(date, 'year') >= 16;
+};
 
 const firstNameSchema = string().required('First Name is required');
 const lastNameSchema = string().required('Last Name is required');
-const dateOfBirthSchema = date().required('Date of Birth is required');
+const dateOfBirthSchema = date()
+  .required('Date of Birth is required')
+  .test('is-min-age', 'You must be at least 16 years old', isMinimumAge);
 
 export const thirdPartyFormSchema = object().shape({
   firstName: firstNameSchema,
