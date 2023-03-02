1b77e0588a981e56c22d06b8478a047dd6cefc21
Ikuni17 <white.brad17@gmail.com>
2023-03-02 16:42:18 -0700

    Add logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import {MantineProvider} from '@mantine/core';
 import {defaultTheme} from './styles/theme';
 import {Router} from './routes';
+import {HugoLogo} from './components';
 
 export const App: React.FC = () => {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={defaultTheme}>
+      {/* Ideally would be in a Header component */}
+      <HugoLogo style={{margin: '8px'}} />
       <Router />
     </MantineProvider>
   );
