5edfb6336fb6034c8fed21c7e98d50ec2edc37b6
Ikuni17 <white.brad17@gmail.com>
2023-03-02 01:17:24 -0700

    Add favico and logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {MantineProvider} from '@mantine/core';
 import {defaultTheme} from './styles/theme';
-import {HugoButton, HugoTextInput} from './components';
+import {HugoButton, HugoContainer, HugoTextInput} from './components';
 
 export const App: React.FC = () => {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={defaultTheme}>
-      <HugoButton>Hello</HugoButton>
-      <HugoTextInput label="World" />
+      <HugoContainer>
+        <HugoButton>Hello</HugoButton>
+        <HugoTextInput label="World" />
+      </HugoContainer>
     </MantineProvider>
   );
 };
