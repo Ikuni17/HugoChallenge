94ebdc7748c140c82289f6651807d55e89429585
Ikuni17 <white.brad17@gmail.com>
2023-03-02 01:04:41 -0700

    Add TextInput component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {MantineProvider} from '@mantine/core';
 import {defaultTheme} from './styles/theme';
-import {HugoButton} from './components';
+import {HugoButton, HugoTextInput} from './components';
 
 export const App: React.FC = () => {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={defaultTheme}>
       <HugoButton>Hello</HugoButton>
+      <HugoTextInput label="World" />
     </MantineProvider>
   );
 };
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,1 +1,2 @@
 export * from './HugoButton';
+export * from './HugoTextInput';
