7737de0fd26755b4cd3ea1d10ef43930fb791997
Ikuni17 <white.brad17@gmail.com>
2023-03-05 14:28:19 -0700

    Add full form validation

--- frontend/src/form/schemaValidation.ts
+++ frontend/src/form/schemaValidation.ts
@@ -1,18 +1,48 @@
 import dayjs from 'dayjs';
-import {object, string, date} from 'yup';
+import {object, string, date, number, array} from 'yup';
 
 const isMinimumAge = (date: Date) => {
   return dayjs().diff(date, 'year') >= 16;
 };
 
-const firstNameSchema = string().required('First Name is required');
-const lastNameSchema = string().required('Last Name is required');
-const dateOfBirthSchema = date()
+const firstName = string().required('First Name is required');
+const lastName = string().required('Last Name is required');
+const dateOfBirth = date()
   .required('Date of Birth is required')
   .test('is-min-age', 'You must be at least 16 years old', isMinimumAge);
+const street = string().required('Street is required');
+const city = string().required('City is required');
+const state = string().required('State is required');
+const zipcode = number().required('Zipcode is required');
+const make = string().required('Make is required');
+const model = string().required('Model is required');
+const vin = string().required('VIN is required');
+const year = number().required('Year is required');
 
-export const thirdPartyFormSchema = object().shape({
-  firstName: firstNameSchema,
-  lastName: lastNameSchema,
-  dateOfBirth: dateOfBirthSchema
+export const thirdPartyFormSchema = object({
+  firstName,
+  lastName,
+  dateOfBirth
+});
+
+export const addressSchema = object({
+  street,
+  city,
+  state,
+  zipcode
+});
+
+export const vehicleSchema = object({
+  make,
+  model,
+  vin,
+  year
+});
+
+export const applicationFormSchema = object({
+  firstName,
+  lastName,
+  dateOfBirth,
+  address: addressSchema,
+  vehicles: array(vehicleSchema).min(1).max(3)
 });
