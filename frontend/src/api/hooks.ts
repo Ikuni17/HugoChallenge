bb705666bed2f33ef3a4e0f8e7512ce26670cdd8
Ikuni17 <white.brad17@gmail.com>
2023-03-05 12:13:14 -0700

    Add application read hook

--- frontend/src/api/hooks.ts
+++ frontend/src/api/hooks.ts
@@ -1,14 +1,30 @@
 import axios from 'axios';
 import {useQuery} from 'react-query';
 
-const apiBase = 'http://localhost:8000/api/';
+type ApplicationIdParam = number | string;
+
+const API_BASE = 'http://localhost:8000/api/';
 
 const listApplications = async () => {
-  const {data} = await axios.get(`${apiBase}application`);
+  const {data} = await axios.get<Application[]>(`${API_BASE}application`);
 
   return data;
 };
 
 export const useApplicationList = () => {
-  return useQuery<Application[]>('applications', listApplications);
+  return useQuery('applications', listApplications);
+};
+
+const getApplicationById = async (applicationId: ApplicationIdParam) => {
+  const {data} = await axios.get<Application>(
+    `${API_BASE}application/${applicationId}`
+  );
+
+  return data;
+};
+
+export const useApplicationRead = (applicationId: ApplicationIdParam) => {
+  return useQuery(['application', applicationId], () =>
+    getApplicationById(applicationId)
+  );
 };
--- frontend/src/views/Application/index.tsx
+++ frontend/src/views/Application/index.tsx
@@ -1,3 +1,10 @@
 import React from 'react';
+import {useParams} from 'react-router-dom';
+import {useApplicationRead} from '../../api';
 
-export const Application: React.FC = () => null;
+export const Application: React.FC = () => {
+  const {id: applicationId} = useParams<{id: string}>();
+  const {data} = useApplicationRead(applicationId);
+
+  return null;
+};
